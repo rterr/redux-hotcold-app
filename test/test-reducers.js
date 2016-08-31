046e038621b129e03b3852c0ee08817f1cf222d0
dillon-mckee <dillon.mckee@gmail.com>
2016-08-31 15:25:37 -0400

    implemented tests for reducers, actions, game component

---
+++ test/test-reducers.js
@@ -0,0 +1,68 @@
+var Reducers = require('../js/reducers');
+var update = require('react-addons-update');
+var React = require('react');
+var Redux = require('redux');
+var TestUtils = require('react-addons-test-utils');
+var should = require('chai').should();
+var Actions = require('../js/actions.js');
+var expect = require('chai').expect();
+
+var initialGameState = {
+  randNum: parseInt(Math.random() * (100) + 1),
+  myNum: 0,
+  numHotness: '',
+  guessCount: 0,
+  guessSet: []
+};
+
+// describe('Reducers', function() {
+//     it('should modify the state based upon the action',  function() {
+//       state = initialGameState;
+//       expect(
+//         Reducers(undefined, {})
+//       ).toEqual([
+//         {
+//           myNum: 0,
+//           numHotness: '',
+//           guessCount: 0,
+//           guessSet: []
+//         }
+//       ])
+//     }
+// });
+
+describe('Testing the reducers', function() {
+    it('return the initial state',  function() {
+      var state = initialGameState;
+      var result = Reducers.gameController(state, {});
+      console.log(result);
+      result.myNum.should.equal(0);
+      result.numHotness.should.equal('');
+      result.guessCount.should.equal(0);
+      result.guessSet.should.be.a.array;
+      result.guessSet.length.should.equal(0);
+    })
+    it('should modify the state based upon the action',  function() {
+      var state = initialGameState;
+      var result = Reducers.gameController(state, Actions.makeGuess(5));
+      result.myNum.should.equal(5);
+      result.numHotness.should.be.a.string;
+      result.guessCount.should.equal(1);
+      result.guessSet.should.be.a.array;
+      result.guessSet.length.should.equal(1);
+    })
+    it('return the game to initial state',  function() {
+        var playedState = {
+          myNum: 25,
+          numHotness: 'Hot',
+          guessCount: 3,
+          guessSet: [1, 2, 3]
+        };
+        var result = Reducers.gameController(playedState, Actions.startNewGame());
+        result.myNum.should.equal(0);
+        result.numHotness.should.equal('');
+        result.guessCount.should.equal(0);
+        result.guessSet.should.be.a.array;
+        result.guessSet.length.should.equal(0);
+    });
+});
